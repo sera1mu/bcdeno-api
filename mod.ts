81ecf810d3e1ab2fc23c9a67b789bbee46e48f53
Seraimu <seraimu.neko@outlook.jp>
2023-03-12 11:23:45 +0900

    chore: add mod.ts

---
+++ mod.ts
@@ -0,0 +1,8 @@
+export { default } from "./src/BCDiceAPIClient.ts";
+export { default as BCDiceOriginalTable } from "./src/BCDiceOriginalTable.ts";
+export type { IBCDiceOriginalTable } from "./src/BCDiceOriginalTable.ts";
+export { default as WebClient } from "./src/WebClient.ts";
+export type { IWebClient } from "./src/WebClient.ts";
+export * from "./src/types/types.ts";
+export * from "./src/types/type_util.ts";
+export * from "./src/types/type_checkers.ts";
