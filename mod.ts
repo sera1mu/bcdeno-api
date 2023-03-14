c1a3186189f867017fc6d3f47e017c394f947da1
Seraimu <seraimu.neko@outlook.jp>
2023-03-14 17:30:38 +0900

    feat: type_util.tsのエクスポートをやめる モジュールの動作と関係ない

--- mod.ts
+++ mod.ts
@@ -4,5 +4,4 @@ export type { IBCDiceOriginalTable } from "./src/BCDiceOriginalTable.ts";
 export { default as WebClient } from "./src/WebClient.ts";
 export type { IWebClient } from "./src/WebClient.ts";
 export * from "./src/types/types.ts";
-export * from "./src/types/type_util.ts";
 export * from "./src/types/type_checkers.ts";
