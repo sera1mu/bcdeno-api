72fca80b7357d4ce56fa5be5775548ed9fb85207
Seraimu <seraimu.neko@outlook.jp>
2023-03-14 17:35:54 +0900

    style: 型のエクスポートをワイルドカードでやる

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 export { default } from "./src/BCDiceAPIClient.ts";
 export { default as BCDiceOriginalTable } from "./src/BCDiceOriginalTable.ts";
-export type { IBCDiceOriginalTable } from "./src/BCDiceOriginalTable.ts";
+export * from "./src/BCDiceOriginalTable.ts";
 export { default as WebClient } from "./src/WebClient.ts";
-export type { IWebClient } from "./src/WebClient.ts";
+export * from "./src/WebClient.ts";
 export * from "./src/types/types.ts";
 export * from "./src/types/type_checkers.ts";
